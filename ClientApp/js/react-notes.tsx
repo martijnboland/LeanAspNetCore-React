04f5b42124509a97c20c97fe70d3ee62dd8f7c42
Martijn Boland <martijn@boland.org>
2018-06-19 15:16:45 +0200

    Added basic React app

---
+++ ClientApp/js/react-notes.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, from React!</h1>,
+  document.getElementById('react-notes-app')
+);
--- ClientApp/webpack.config.js
+++ ClientApp/webpack.config.js
@@ -9,12 +9,17 @@ module.exports = (env = {}, argv = {}) => {
     mode: argv.mode || 'development', // we default to development when no 'mode' arg is passed
     entry: {
       main: './js/main.js',
-      classic_jquery: './js/classic-jquery.js'
+      classic_jquery: './js/classic-jquery.js',
+      react_notes: './js/react-notes.tsx'
     }, 
     output: {
       filename: '[name].js',
       path: path.resolve(__dirname, '../wwwroot/dist'),
       publicPath: "/dist/"
+    },
+    resolve: {
+      // Add `.ts` and `.tsx` as a resolvable extension.
+      extensions: [".ts", ".tsx", ".js"]
     },
     plugins: [
       new MiniCssExtractPlugin({
@@ -29,10 +34,19 @@ module.exports = (env = {}, argv = {}) => {
             isProd ?  MiniCssExtractPlugin.loader : 'style-loader', 
             'css-loader' 
           ]
+        },
+        {
+          test: /\.tsx?$/,
+          use: 'awesome-typescript-loader',
+          exclude: /node_modules/
         }
       ]
     }
   }
   
+  if (! isProd) {
+    config.devtool = 'eval-source-map';
+  }
+
   return config;
 };
