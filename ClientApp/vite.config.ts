b99481d6d498dea590f8fd036b096b9b21ca5129
Martijn Boland <martijn@taiga.nl>
2021-02-23 14:54:29 +0100

    Revert "Client-side development files are now server from ClientApp to make a clear distinction from production bundles."
    This reverts commit 4493c632dd64c9c2db0e3993b51f0c20f3c20919.
    
    Unfortunately, the Vite base folder (ClientApp) is also compiled into production bundles to reference images. Set back to '/dist/' to match production bundle output folders.

--- ClientApp/vite.config.ts
+++ ClientApp/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
-  base: '/ClientApp/',
+  base: '/dist/',
   build: {
     outDir: '../wwwroot/dist',
     emptyOutDir: true,
